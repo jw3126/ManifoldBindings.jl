3d60c0d05061fb669e5b0a64902424ff3fbb4145
Jan Weidner <jw3126@gmail.com>
2024-05-22 09:38:09 +0200

    cleanup

--- src/ManifoldBindings.jl
+++ src/ManifoldBindings.jl
@@ -2,18 +2,10 @@ module ManifoldBindings
 using Libdl: dlopen
 using ArgCheck: @argcheck, @check
 using Base.Libc
-using manifold_jll
+import manifold_jll
 
 include("capi.jl")
 include("highlevel.jl")
 include("makie.jl")
-
-
-# function __init__()
-#     path = "manifold/build/bindings/c/libmanifoldc.so"
-#     # path= "/home/jan/products/lib/libmanifoldc.so"
-#     @argcheck isfile(path)
-#     dlopen(path)
-# end
 
 end
